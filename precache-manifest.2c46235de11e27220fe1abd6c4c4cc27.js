9b5e2671c4dddf2cd23696d8e0b0af7839885774
Dustin Downs <2736672+dustin-downs@users.noreply.github.com>
2020-03-25 20:11:54 +0800

    Updates

---
+++ precache-manifest.2c46235de11e27220fe1abd6c4c4cc27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b597471c71bf5716806e6d1906d3b8dc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3dde6a6f63a42d22a05e",
+    "url": "/monsters-rolodex/static/css/main.5a991a3f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dde6a6f63a42d22a05e",
+    "url": "/monsters-rolodex/static/js/main.02f8d796.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
